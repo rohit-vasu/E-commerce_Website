0e3ac77ea4b66823333d87632339fa7f82402748
Shashika Danasekara <shashika.danasekara2@outlook.com>
2021-03-21 13:31:35 +0530

    basic html templates

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'store.apps.StoreConfig'
 ]
 
 MIDDLEWARE = [
@@ -47,8 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    'store.apps.StoreConfig'
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
