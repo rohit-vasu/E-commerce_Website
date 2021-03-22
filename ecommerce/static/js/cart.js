e94b1f391ed2c87a115fdda2370ccaa0242f4b2d
Shashika Danasekara <shashika.danasekara2@outlook.com>
2021-03-23 00:02:21 +0530

    add to cart functionality 1

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -6,5 +6,46 @@ for (i=0 ; i<updateBtns.length;i++){
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId :',productId,'Action',action)
+
+        console.log('user : ',user)
+        if (user == 'AnonymousUser'){
+            console.log('User is not signed in..')
+        }
+        else{
+            console.log('User is signed in..')
+            updateUserOrder(productId,action)
+        }
+
     })
 }
+
+function updateUserOrder(productId,action){
+    console.log('Sending Data..')
+
+    var url = '/update_item/'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body: JSON.stringify({'productId': productId,'action':action})
+    })
+
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log('data : ',data)
+    })
+
+}
+
+
+
+
+
+
+ 
+
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import  *
 
 # Create your views here.
@@ -30,3 +32,6 @@ def checkout(request):
 
     context = {'items':items,'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item Was added', safe=False)
