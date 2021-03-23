3798891254f20d0bd15b61c7b615f60e3e1a1378
Shashika Danasekara <shashika.danasekara2@outlook.com>
2021-03-23 12:37:43 +0530

    shipping form logic

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -18,7 +18,7 @@ def store(request):
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
-    context = {'products':products,'cartItems':cartItems}
+    context = {'products':products,'cartItems':cartItems,'shipping':False}
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -29,7 +29,7 @@ def cart(request):
         cartItems= order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0,'get_cart_items':0}
+        order = {'get_cart_total':0,'get_cart_items':0,'shipping':False}
         cartItems = order['get_cart_items']
 
     context = {'items':items,'order':order,'cartItems':cartItems}
@@ -43,7 +43,7 @@ def checkout(request):
         cartItems= order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0,'get_cart_items':0}
+        order = {'get_cart_total':0,'get_cart_items':0,'shipping':False}
         cartItems = order['get_cart_items']
 
     context = {'items':items,'order':order,'cartItems':cartItems}
