7479f5094ffe2ae0b062c6796ac839247d0b5c7b
Shashika Danasekara <shashika.danasekara2@outlook.com>
2021-03-29 10:39:31 +0530

    completed login page

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -128,3 +128,12 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
+#smtp config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ecom4web.team24@gmail.com'
+EMAIL_HOST_PASSWORD = '#SdRvSe#'
+
+
