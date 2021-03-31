8b35d8ae06cb649200f9ae9ebbb11744670217b9
Shashika Danasekara <shashika.danasekara2@outlook.com>
2021-04-01 01:06:51 +0530

    search page

--- ecommerce/store/filter.py
+++ ecommerce/store/filter.py
@@ -4,6 +4,7 @@ from .models import *
 
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(label='Key-Word',lookup_expr='icontains')
+    price = django_filters.NumberFilter(label='Price',field_name="price", lookup_expr='lte')
     class Meta:
         model = Product
         fields = ['name','price', 'tags']
